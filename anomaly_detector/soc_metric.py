a7e39336c5d7d248f8b2364985f14e040c5344b8
suessmeister <josephsuess24@gmail.com>
2024-07-11 12:48:07 -0500

    version 1.2.1 -- testing gridsearchcv

--- anomaly_detector/soc_metric.py
+++ anomaly_detector/soc_metric.py
@@ -42,27 +42,60 @@ def preprocess_data():
     encoder = LabelEncoder()
     df.iloc[:, 0] = encoder.fit_transform(df.iloc[:, 0])
 
+    # now for funsies, let's graph this data so we might find some anomalies using our eyes alone.
+    print(df.to_string())
+    plt.figure()
+    df.plot()
+
+
 def create_model():
-    model = LocalOutlierFactor(n_neighbors=5)
-    model.fit(df)
-    scores = model.negative_outlier_factor_
-    outliers = np.argwhere(scores > np.percentile(scores, 95))
+    pipe = Pipeline([
+        ("scale", StandardScaler()),
+        ("model", LocalOutlierFactor())
+    ])
 
-    colors = ['green', 'red']
+    # find potential parameters in the pipeline.
+    print(pipe.get_params())
 
-    for i in range(len(df)):
-        if i not in outliers:
-            plt.scatter(df.iloc[i, 0], df.iloc[i, 1], color=colors[0])  # Not anomly
-        else:
-            plt.scatter(df.iloc[i, 0], df.iloc[i, 1], color=colors[1])  # anomly
-    plt.title('Local Outlier Factor', fontsize=16)
-    plt.show()
+    parameter_grid = {
+        'model__n_neighbors': np.arange(start=1, stop=11, step=1).tolist(),
+        'model__contamination': np.arange(start=0.1, stop=0.5, step=0.05).tolist()
+    }
 
+    grid = GridSearchCV(
+        estimator='pipe'
+    )
+    # now fit to the data.
+    pipe.fit(df)
+
+    # get the outlier scores for the model
+    outlier_scores = pipe.named_steps['model'].negative_outlier_factor_
+
+    # define what an outlier is
+    outliers = df[outlier_scores < -3]
+
+    print(outliers)
+
+    # print(scores)
+    # outliers = np.argwhere(scores > np.percentile(scores, 95))
+    # print(outliers)
+    # colors = ['green', 'red']
+    # plt.show()
+
+    # for i in range(len(df)):
+    #     if i not in outliers:
+    #         plt.scatter(df.iloc[i, 0], df.iloc[i, 1], color=colors[0])  # Not anomly
+    #     else:
+    #         plt.scatter(df.iloc[i, 0], df.iloc[i, 1], color=colors[1])  # anomly
+    # plt.title('Local Outlier Factor', fontsize=16)
+    # plt.show()
+    # return 0
 
 
 def main():
     create_incidents()
     preprocess_data()
     create_model()
+
 
 main()
