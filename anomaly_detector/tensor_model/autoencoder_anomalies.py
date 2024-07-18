990cfa5c25352173d62866a0fef4329d157e1522
suessmeister <josephsuess24@gmail.com>
2024-07-17 19:59:53 -0500

    pivoting to tensorflow! ver 2.0

--- anomaly_detector/tensor_model/autoencoder_anomalies.py
+++ anomaly_detector/tensor_model/autoencoder_anomalies.py
@@ -1,14 +1,27 @@
 # gathering all the imports
-import pandas as pd # for dataframe objects
 
+from tabulate import tabulate # for visualizing the pandas dataframe better. ipython can also be used?
+import pandas as pd # for dataframe objects
+import re # regular expression operations for strings.. used to tokenize the words without using vectors.
+
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split # splitting the data accordingly
 
-
 # Data Preprocessing - Making this data usable
+
+
 raw_data = pd.read_excel("../incidents_jan_2024.xlsx") # 758 "rows" or incidents and 18 categorical columns
 
 names = raw_data.iloc[:, 0] # get only the incident names for the dataframe
-print(names)
+
+updated_data = pd.DataFrame()
+updated_data['Raw Names'] = [str(names)] # add a column to this DataFrame storing the initial names. needs to be string.
+updated_data['Lowercase Names'] = updated_data['Raw Names'].apply(lambda word: word.lower()) # make lowercase column.
+print(updated_data.to_string())
+
+
+
+
 
 
 
