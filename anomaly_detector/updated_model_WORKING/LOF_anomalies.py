37db8c97e64a99af36b7226a661f990d12d8b6f0
suessmeister <josephsuess24@gmail.com>
2024-07-19 00:39:58 -0500

    ver 1.2 everything up to date? adding visualizations in 1.3

--- anomaly_detector/updated_model_WORKING/LOF_anomalies.py
+++ anomaly_detector/updated_model_WORKING/LOF_anomalies.py
@@ -1,42 +1,46 @@
 # gathering all the imports
+import incident  # for defining the blueprint of an incident
 
 # data preprocessing imports required
-from tabulate import tabulate # for visualizing the pandas dataframe better. ipython can also be used?
-import pandas as pd # for dataframe objects
-import re # regular expression operations for strings. used to tokenize the words without using vectors.
-from nltk.corpus import stopwords # for getting rid of the stopwords AKA words that are not needed for the model
+from tabulate import tabulate  # for visualizing the pandas dataframe better. ipython can also be used?
+import pandas as pd  # for dataframe objects
+import re  # regular expression operations for strings. used to tokenize the words without using vectors.
+from nltk.corpus import stopwords  # for getting rid of the stopwords AKA words that are not needed for the model
 
-
-
-from sklearn.feature_extraction.text import TfidfVectorizer # assign tfidf scores in the model
-from sklearn.neighbors import LocalOutlierFactor # the machine learning model to use. this is unsupervised learning.
+from sklearn.feature_extraction.text import TfidfVectorizer  # assign tfidf scores in the model
+from sklearn.neighbors import LocalOutlierFactor  # the machine learning model to use. this is unsupervised learning.
 
 # Data Preprocessing - Making this data usable
 
 
-raw_data = pd.read_excel("../incidents_jan_2024.xlsx") # 758 "rows" or incidents and 18 categorical columns
+raw_data = pd.read_excel("../incidents_jan_2024.xlsx")  # 758 "rows" or incidents and 18 categorical columns
 
-names = raw_data.iloc[:, 0] # get only the incident names for the dataframe
+# Defining the proper functions while also instantiating from the Incident Class.
+def create_incidents():
+    processed_incidents = []
+    for index, raw_incident in raw_data.iterrows():
+        df_array = raw_incident.to_numpy()
 
-updated_data = pd.DataFrame()
-updated_data['Raw Names'] = names # add a column to this DataFrame storing the initial names. needs to be string.
-updated_data['Lowercase Names'] = updated_data['Raw Names'].apply(lambda word: word.lower()) # make lowercase column.
+        # nice, these parameters work when incident string method is initialized!
+        # define an initial threat index to 0 for each incident.
+        processed_incident = incident.Incident(df_array[0], df_array[1], df_array[3],
+                                               df_array[6], df_array[7],
+                                               df_array[15], df_array[16],
+                                               threat_index=0)
 
+        processed_incidents.append(processed_incident)
+    return processed_incidents
+
+
+# Tokenizing the data. Used in lambda function call.
 def tokenizer(str):
     tokens = re.split("\W+", str)
     return tokens
 
 
-# add a column containing the tokens from each incident name. once again using lambda functions for simplicity.
-updated_data['Tokenized Names'] = updated_data['Lowercase Names'].apply(lambda sentence: tokenizer(sentence))
-
-stop_words = set(stopwords.words('english')) # get the stopwords for the english dictionary
-
-# NOTE!!! This requires you to open a Python Interpreter and use the following commands:
-# import nltk
-# nltk.download('stopwords')
-
-def clean_stopwords(token_list): # getting rid of stopwords while also getting rid of tokens that start with numbers
+# Getting rid of stopwords while also removing unnecessary numbers (these should NOT be weighted).
+# Used in lambda function call.
+def clean_stopwords(token_list):  # getting rid of stopwords while also getting rid of tokens that start with numbers
     new_tokens = ""
     for token in token_list:
         if token not in stop_words and (not token[0].isdigit() if token else True):
@@ -44,32 +48,46 @@ def clean_stopwords(token_list): # getting rid of stopwords while also getting r
     return new_tokens
 
 
+# Data Preprocessing - lowercase, tokenizations, removing stopwords, vectorizing tokens, assigning TF_IDF scores
+
+names = raw_data.iloc[:, 0]  # get only the incident names for the dataframe
+
+updated_data = pd.DataFrame()
+updated_data['Raw Names'] = names  # add a column to this DataFrame storing the initial names. needs to be string.
+updated_data['Lowercase Names'] = updated_data['Raw Names'].apply(lambda word: word.lower())  # make lowercase column.
+
+# add a column containing the tokens from each incident name. once again using lambda functions for simplicity.
+updated_data['Tokenized Names'] = updated_data['Lowercase Names'].apply(lambda sentence: tokenizer(sentence))
+
+stop_words = set(stopwords.words('english'))  # get the stopwords for the english dictionary
+
+# NOTE!!! This requires you to open a Python Interpreter and use the following commands:
+# import nltk
+# nltk.download('stopwords')
+
 # clean out words that do not contribute to the model
 updated_data['No Stopwords'] = updated_data['Tokenized Names'].apply(lambda tokens: clean_stopwords(tokens))
 
-
 vectorizer = TfidfVectorizer()
-fitted_X = vectorizer.fit_transform(updated_data['No Stopwords']) # learn the vocab and IDF scores
-# print(vectorizer.get_feature_names_out())
-# print(vectorizer.idf_)
+fitted_X = vectorizer.fit_transform(updated_data['No Stopwords'])  # learn the vocab and IDF scores
 
+
+# Local Outlier Factor Model Creation and Fitting
 
 model = LocalOutlierFactor()
-model.fit(fitted_X) # fit the model on the given data
-outlier_scores = model.negative_outlier_factor_ # find local outliers. -1 is a constant value and indicates an inlier
-updated_data['Outlier Scores'] = outlier_scores # outliers further away from -1 are more likely to be outliers.
-updated_data = updated_data.sort_values('Outlier Scores') # descending order!
+model.fit(fitted_X)  # fit the model on the given data
+outlier_scores = model.negative_outlier_factor_  # find local outliers. -1 is a constant value and indicates an inlier
+updated_data['Outlier Scores'] = outlier_scores  # outliers further away from -1 are more likely to be outliers.
+updated_data = updated_data.sort_values('Outlier Scores')  # descending order!
 
 
-print(tabulate(updated_data, headers='keys', tablefmt='psql')) # shoutout to tabulate for such a lightweight tool!
+# Incident Matching or Data Preparation for Visualizations
+
+# incidents now contains each incident in chronological order
+incidents = create_incidents()
+test = updated_data.at[1, 'Raw Names']
+print(updated_data.index)
+print(test)
 
 
-
-
-
-
-
-
-
-
-
+# print(tabulate(updated_data.head(), headers='keys', tablefmt='psql'))  # shoutout to tabulate for such a lightweight tool!
---
+++ anomaly_detector/updated_model_WORKING/incident.py
@@ -0,0 +1,26 @@
+# defining the blueprint for creating an incident object, i.e. one row in the excel sheet.
+# has different methods depending on current and/or future needs
+class Incident:
+    def __init__(self, name, id, severity, accounts_reached, impact, classification, determination, threat_index):
+        self.name = name
+        self.id = id
+        self.severity = severity
+        self.accounts_reached = accounts_reached
+        self.impact = impact
+        self.classification = classification
+        self.determination = determination
+
+        # real/unknown incidents marked as a '1' else do not mark as an active incident.
+        self.threat_index = 1 if self.classification in {"True alert", "Benign Positive", "Not set"} else 0
+
+    def return_raw_data(self):
+        return [
+            self.severity,
+            self.impact,
+            self.name
+        ]
+
+    def __str__(self):
+        return (f"incident {self.name} with id {self.id} and severity {self.severity} and impact {self.impact}"
+                f" with determination {self.determination}")
+
